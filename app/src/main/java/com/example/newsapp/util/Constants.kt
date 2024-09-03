680ea4590ebdcc0374b0700f32204c18cf9ebf05
BikashGupta-Alfaleus <bikash.alfaleus@gmail.com>
2024-09-03 22:48:22 +0530

    Added code for fragments and NewsActivity

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
     companion object{
         const val API_KEY = "bea27ec5dc10418bb50a420011a8ef09"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY ="500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
