e8b1e6423974669da9f7b328bf678b2737334d66
BikashGupta-Alfaleus <bikash.alfaleus@gmail.com>
2024-09-05 23:03:54 +0530

    Error fixes

--- app/src/main/java/com/example/newsapp/models/Article.kt
+++ app/src/main/java/com/example/newsapp/models/Article.kt
@@ -2,6 +2,7 @@ package com.example.newsapp.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(
     tableName = "articles"
@@ -10,11 +11,11 @@ data class Article(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
     val author: String,
-    val content: Any,
+    val content: String,
     val description: String,
     val publishedAt: String,
     val source: Source,
     val title: String,
     val url: String,
-    val urlToImage: Any
-)
+    val urlToImage: String
+) : Serializable
